bd95b7645ef11b2983ee18eb26f1b53a5279caa8
afashag <07reg@2000@gmail.com>
2024-01-24 21:51:28 +0300

    24.01.2024

--- js/index.js
+++ js/index.js
@@ -24,4 +24,33 @@ document.addEventListener('DOMContentLoaded', () => {
           el: '.swiper-scrollbar',
         },
       });
+
+      let color = document.querySelector('.calc__right-color')
+      let cveta = document.querySelector('.calc__right-cveta')
+      
+      let white = document.querySelector('.calc__right-white')
+      let brown = document.querySelector('.calc__right-brown')
+      let antra = document.querySelector('.calc__right-antra')
+
+      const handleClick = () => {
+        cveta.classList.toggle('calc__right-cveta-active')
+      }
+
+      color.addEventListener('click', handleClick)
+
+      white.addEventListener('click', () => {
+        console.log('white');
+      })
+      brown.addEventListener('click', () => {
+        console.log('brown');
+      })
+      antra.addEventListener('click', () => {
+        console.log('antra');
+      })
 })
+
+      
+
+
+      
+      
