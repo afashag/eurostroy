3430473275bd5dc7d7a3bf0328b51896394f8ca7
afashag <07reg@2000@gmail.com>
2024-01-25 21:58:41 +0300

    25/01/2024

--- js/index.js
+++ js/index.js
@@ -25,32 +25,89 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       });
 
-      let color = document.querySelector('.calc__right-color')
-      let cveta = document.querySelector('.calc__right-cveta')
+      let inputs = document.querySelectorAll('input') //все инпуты
+
+      let width = document.querySelector('.calc__left-width') //ширина 
+      let height = document.querySelector('.calc__left-height') // высота
+
+      let res = document.querySelector('.calc__right-res') // див, где стоимость
+
+
+      let color = document.querySelector('.calc__right-color') // див, где слово цвет(нажимаешь на него и выпадает список)
+      let cveta = document.querySelector('.calc__right-cveta') // див, где выпадающий список
       
-      let white = document.querySelector('.calc__right-white')
-      let brown = document.querySelector('.calc__right-brown')
-      let antra = document.querySelector('.calc__right-antra')
+      
+
+
+      let cvetaType = document.querySelectorAll('input[name="cveta"]') // нахожу инпуты цвета
+      let dost = document.querySelector('input[name="dost"]') // нахожу доставку
+      let ust = document.querySelector('input[name="ust"]') // нахожу установку
+
+      for (const input of inputs){
+        input.addEventListener('input', function (){
+          formula()
+        })
+      } //цикл, который проверяет произошли ли изменения в инпутах
+
+      function formula(){
+        let totalPrice = (+height.value * +width.value)*1000
+        
+
+        for (const radio of cvetaType){
+          if (radio.checked === true){
+            totalPrice = (+height.value * +width.value)* parseInt(radio.value)
+          } // проверка на то какой цвет выбран
+
+          if (dost.checked === true){
+            totalPrice = (+height.value * +width.value)* parseInt(radio.value) + 200
+          } // проверка на доставку
+
+          if (ust.checked === true){
+            totalPrice = (+height.value * +width.value)* parseInt(radio.value) + 200
+          } // проверка на установку
+
+          else {
+            console.log("fksdjaf;laksd");
+          }
+          // сделать так чтобы всегда проверял
+
+          //сделать форматтер
+
+          
+      } // формула расчета цены
+        res.innerHTML = totalPrice
+      
+      }
+
+      // formula() // если нужно чтобы подсчет производился сразу
+
+      
 
       const handleClick = () => {
         cveta.classList.toggle('calc__right-cveta-active')
-      }
+      } // при клике на див color выпадает меню спиок со всеми цветами
 
-      color.addEventListener('click', handleClick)
+      color.addEventListener('click', handleClick) // вешаю событие на див со словом цвет
 
-      white.addEventListener('click', () => {
-        console.log('white');
-      })
-      brown.addEventListener('click', () => {
-        console.log('brown');
-      })
-      antra.addEventListener('click', () => {
-        console.log('antra');
-      })
+// ----------------------------------------------------------------------------------------------------
+      let white = document.querySelector('.calc__right-white') // див, где белый
+      let brown = document.querySelector('.calc__right-brown') // див, где корич
+      let antra = document.querySelector('.calc__right-antra') // див, где антра
+
+      // white.addEventListener('click', () => {
+      //   console.log('white');
+      // })
+      // brown.addEventListener('click', () => {
+      //   console.log('brown');
+      // })
+      // antra.addEventListener('click', () => {
+      //   console.log('antra');
+      // })
+          
 })
 
       
 
-
+// ((B3*C3)*1000)+((B3*D3)*100)+(E3*100)+(G3*200)+(H3*200)
       
       
