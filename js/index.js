991f5672ac7e3147d7421b4ece742737748b0c33
afashag <07reg@2000@gmail.com>
2024-02-09 16:53:26 +0300

    09.02.2024year

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-
 
     new Swiper('.swiper', {
         // Optional parameters
@@ -28,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       });
 
+
       let inputs = document.querySelectorAll('input') //все инпуты
 
       let width = document.querySelector('.calc__right-width') //ширина 
@@ -35,26 +34,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
       let res = document.querySelector('.calc__right-res') // див, где стоимость
 
-
-      let color = document.querySelector('.calc__right-color') // див, где слово цвет(нажимаешь на него и выпадает список)
-      let cveta = document.querySelector('.calc__right-cveta') // див, где выпадающий список
-      
-
-
-      let cvetaType = document.querySelectorAll('input[name="cveta"]') // нахожу инпуты цвета
-
+      let colorsType = document.querySelectorAll('input[name="colors"]') // нахожу инпуты цвета
 
       let dost = document.querySelector('input[name="dost"]') // нахожу доставку чекбокс
       let ust = document.querySelector('input[name="ust"]') // нахожу установку чекбокс
       let poperek = document.querySelector('input[name="poperek"]') // нахожу поперек чекбокс
       let derzhat = document.querySelector('input[name="derzhat"]') // нахожу держатель чекбокс
-      
+
+      let bottomInputs = document.querySelectorAll('.calc__right-input')
+      let bottomDivs = document.querySelectorAll('.calc__right-text')
+
+      for (const item of bottomInputs){
+        item.addEventListener('click', function (event){
+          if (event.currentTarget.id === "1"){
+            bottomDivs[0].classList.toggle('calc__right-text-active')
+            bottomDivs[1].classList.remove('calc__right-text-active')
+            bottomDivs[2].classList.remove('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "2"){
+            bottomDivs[0].classList.remove('calc__right-text-active')
+            bottomDivs[1].classList.toggle('calc__right-text-active')
+            bottomDivs[2].classList.remove('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "3"){
+            bottomDivs[0].classList.remove('calc__right-text-active')
+            bottomDivs[1].classList.remove('calc__right-text-active')
+            bottomDivs[2].classList.toggle('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "4"){
+            bottomDivs[3].classList.toggle('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "5"){
+            bottomDivs[4].classList.toggle('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "6"){
+            bottomDivs[5].classList.toggle('calc__right-text-active')
+          }
+          if (event.currentTarget.id === "7"){
+            bottomDivs[6].classList.toggle('calc__right-text-active')
+          }
+      })
+     }
+
+
 
       function formula(){
 
         let totalPrice = (+height.value * +width.value)*1000 //рассчет
         
-          for (const radio of cvetaType){
+          for (const radio of colorsType){
 
             if (radio.checked === true){
               totalPrice = (+height.value * +width.value)* parseInt(radio.value)
@@ -77,7 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             
           const formatter = new Intl.NumberFormat('ru');
-          res.innerHTML = formatter.format(totalPrice) //форматтер + // внос значения в див рес
+          res.innerHTML = formatter.format(totalPrice) + " ₽" //форматтер + // внос значения в див рес
       } 
 
       // formula() // если нужно чтобы подсчет производился сразу
@@ -90,16 +118,11 @@ document.addEventListener('DOMContentLoaded', () => {
       } //цикл, который проверяет произошли ли изменения в инпутах
       
 
-      // const handleClick = () => {
-      //   cveta.classList.toggle('calc__right-cveta-active')
-      // } // при клике на див color выпадает меню спиок со всеми цветами
-
-      // color.addEventListener('click', handleClick) // вешаю событие на див со словом цвет
           
 })
 
       
 
-// ((B3*C3)*1000)+((B3*D3)*100)+(E3*100)+(G3*200)+(H3*200)
+
       
       
